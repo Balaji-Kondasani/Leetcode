5691c2b64af4a73bd2785cfdca5289e966a71d7d
KBINDIA <isroindia647@gmail.com>
2024-12-10 17:53:25 +0530

    Standard Array Problem

---
+++ Arrays/koko_eating_bananas.py
@@ -0,0 +1,54 @@
+#Leetcode 53
+#MEDIUM
+
+
+#BRUTE FORCE APPROACH
+#SOLUTION
+def canKokoPlace(self,piles,div,h):
+        originalHours=0
+        for i in piles:
+            originalHours+=i//div
+            if i%div!=0:
+                originalHours+=1
+        return originalHours<=h
+    
+       
+    def minEatingSpeed(self, piles, h):
+        min_eating_speed=float('inf')
+        for i in range(1,max(piles)+1):
+            res=self.canKokoPlace(piles,i,h)
+            if res:
+                min_eating_speed=min(min_eating_speed,i)
+        return min_eating_speed
+#Time Complexity -- O(n * m), where n is the number of piles and m is the maximum number of bananas in a single pile. 
+#Space Complexity -- O(1)
+#Leetcode Verdict -- Time Limit Exceeded
+
+
+#OPTIMAL APPROACH 
+#SOLUTION
+class Solution(object):
+    def canKokoPlace(self,piles,mid,h):
+        originalHours=0
+        for i in piles:
+            originalHours+=i//mid
+            if i%mid!=0:
+                originalHours+=1
+        return originalHours<=h
+    
+       
+    def minEatingSpeed(self, piles, h):
+        left=1
+        right=max(piles)
+        while left<right:
+            mid=left+(right-left)//2
+            res=self.canKokoPlace(piles,mid,h)
+            if res:
+                right=mid
+            else:
+                left=mid+1
+        return left                
+#Time Complexity -- O(nlog(m)) ---> where n is the number of piles and m is the maximum number of bananas in a single pile.
+#Space Complexity -- O(1)
+#Leetcode Verdict -- Accepted
+
